8b488abce8df2fbf4b720d005983008a8b45bfc5
Dante Allievi <toto99.-@hotmail.com>
2020-11-17 21:56:42 +0100

    Levels screen added.

--- game.js
+++ game.js
@@ -1,20 +1,46 @@
 import {
   update as updateSnake,
   draw as drawSnake,
-  SNAKE_SPEED,
   getSnakeHead,
   snakeIntersection,
 } from './snake.js';
 import { update as updateFood, draw as drawFood } from './food.js';
 import { outsideGrid } from './grid.js';
 
-let lastRenderTime = 0;
-let gameOver = false;
+// HTML Elements
+const easyBtn = document.getElementById('easy');
+const mediumBtn = document.getElementById('medium');
+const hardBtn = document.getElementById('hard');
+const introScreen = document.querySelector('.intro');
 const gameBoard = document.getElementById('game-board');
 
+// Variables
+let SNAKE_SPEED = 1;
+let lastRenderTime = 0;
+let gameOver = false;
+
+// Main Function
 function main(currentTime) {
+  easyBtn.addEventListener('click', () => {
+    introScreen.classList.add('fadeOut');
+    gameBoard.classList.add('fadeIn');
+    SNAKE_SPEED = 5;
+  });
+
+  mediumBtn.addEventListener('click', () => {
+    introScreen.classList.add('fadeOut');
+    gameBoard.classList.add('fadeIn');
+    SNAKE_SPEED = 10;
+  });
+
+  hardBtn.addEventListener('click', () => {
+    introScreen.classList.add('fadeOut');
+    gameBoard.classList.add('fadeIn');
+    SNAKE_SPEED = 16;
+  });
+
   if (gameOver) {
-    if (confirm('You lost. Press OK to restart.')) {
+    if (confirm('You lost. Press OK to try again.')) {
       window.location = '/';
     }
     return;
@@ -30,6 +56,8 @@ function main(currentTime) {
 }
 
 window.requestAnimationFrame(main);
+
+// Functions
 
 function update() {
   updateSnake();
